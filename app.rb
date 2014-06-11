c641ee4d4a36bbe49a39ed8840128e961d2664aa
Hitesh Sharma <hitesh@slideshare.com>
2014-06-11 19:03:51 +0530

    bakchodi 1

--- app.rb
+++ app.rb
@@ -2,16 +2,16 @@ require 'sinatra'
 
 get '/' do
   "<a href='/write-test'>Test Write in iframe</a><br/>
-<a href='https://bkgxzlhytq.localtunnel.me/set-cookie' target='_blank'>Set 3rd party cookie in other window</a><br/>
+<a href='http://ec2-23-20-231-145.compute-1.amazonaws.com:3000//set-cookie' target='_blank'>Set 3rd party cookie in other window</a><br/>
 <a href='/read-test'>Test read in iframe</a><br/>
-<a href='https://bkgxzlhytq.localtunnel.me/test-redirect'>Test cookie via redirect</a><br/>
-<a href='https://bkgxzlhytq.localtunnel.me/clear-cookie'>clear 3rd party cookie<a>
+<a href='http://ec2-23-20-231-145.compute-1.amazonaws.com:3000//test-redirect'>Test cookie via redirect</a><br/>
+<a href='http://ec2-23-20-231-145.compute-1.amazonaws.com:3000//clear-cookie'>clear 3rd party cookie<a>
 "
 end
 get '/write-test' do
-"<p>iframe below</p><iframe src='https://bkgxzlhytq.localtunnel.me/testwrite' width=500 height=500></iframe>"
+"<p>iframe below</p><iframe src='http://ec2-23-20-231-145.compute-1.amazonaws.com:3000//testwrite' width=500 height=500></iframe>"
 end
 
 get '/read-test' do
-  "<p>iframe below</p><iframe src='https://bkgxzlhytq.localtunnel.me/test-read' width=500 height=500></iframe>"
+  "<p>iframe below</p><iframe src='http://ec2-23-20-231-145.compute-1.amazonaws.com:3000//test-read' width=500 height=500></iframe>"
 end
